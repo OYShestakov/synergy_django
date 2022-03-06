f79a01d45a76cb14268e2284f2f73a1a7537d083
Олег Шестаков <olegsestakov@MacBook-Air-Oleg.local>
2022-03-06 14:00:23 +0600

    add image

--- service/views.py
+++ service/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, Http404
+from django.http import HttpResponse, Http404, FileResponse, JsonResponse
 from django.urls import reverse
 
 from .models import Product
 from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import get_template
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello world! on service')
+    # template = get_template('index.html')
+    return render(request, 'index.html')
 
 
 def page(request, page_num):
@@ -18,3 +21,7 @@ def about(request, id):
         raise Http404('Not found its my text')
 
     return HttpResponse('Ok')
+
+def json_show(request):
+    data = {'cost':14, 'title':'book'}
+    return JsonResponse(data)
