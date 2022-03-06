391fe343ec313876c5bb8e04b2f768651ceac315
Олег Шестаков <olegsestakov@MacBook-Air-Oleg.local>
2022-03-06 12:24:24 +0600

    Init app

--- service/views.py
+++ service/views.py
@@ -1,8 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, Http404
+from django.urls import reverse
+
+from .models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('Hello world! on service')
 
 
 def page(request, page_num):
     return HttpResponse(f'Page {page_num}')
+
+def about(request, id):
+    try:
+        var = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        raise Http404('Not found its my text')
+
+    return HttpResponse('Ok')
