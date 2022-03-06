d1941ecf5a2f9b619d3717e1c25bc5c26f1eef89
Олег Шестаков <olegsestakov@MacBook-Air-Oleg.local>
2022-03-06 15:17:04 +0600

    lesson 09

--- service/admin.py
+++ service/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
-from .models import Product, Bin
+from .models import Product, Bin, Home
 
 # admin.site.register(Product)
 admin.site.register(Bin)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id', 'coast']
-    fields = ['id']
-    list_filter = ['coast']
-    search_fields = ['title']
+    list_display = ['title', 'id', 'coast', 'is_active']
+    # fields = ['id']
+    # list_filter = ['coast']
+    # search_fields = ['title']
 
 # admin.site.register(Product, ProductAdmin)
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ['size', 'coast', 'adr', 'bal', 'is_active']
+    # fields = ['id']
+    # list_filter = ['coast']
+    # search_fields = ['title']
--- service/models.py
+++ service/models.py
@@ -1,3 +1,4 @@
+import null
 from django.db import models
 from django.db.models import Model
 
@@ -7,6 +8,7 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     coast = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
+    is_active = models.BooleanField(verbose_name='Активен', default=True, null=True)
 
     def __str__(self):
         return self.title
@@ -20,3 +22,15 @@ class Bin(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Home(models.Model):
+
+    size = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Размер', null=True)
+    coast = models.IntegerField(verbose_name='Цена', null=True)
+    adr = models.CharField(max_length=200, verbose_name='Адрес', default='Армовирская', null=True)
+    bal = models.BooleanField(verbose_name='Балкон', default=True)
+    is_active = models.BooleanField(verbose_name='Активен', default=True, null=True)
+
+    def __str__(self):
+        return self.adr
